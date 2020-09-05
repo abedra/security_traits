552527075008a87da6fc80812214c6b187ac61c3
Aaron Bedra <aaron@aaronbedra.com>
2020-09-05 09:18:38 -0500

    Refactor

--- src/main/java/com/aaronbedra/web/traits/SecureRedirect.java
+++ src/main/java/com/aaronbedra/web/traits/SecureRedirect.java
@@ -1,32 +1,31 @@
 package com.aaronbedra.web.traits;
 
 import com.aaronbedra.web.WebRequester;
-import com.jnape.palatable.lambda.adt.Unit;
 import com.jnape.palatable.lambda.functions.specialized.SideEffect;
 import com.jnape.palatable.lambda.io.IO;
 import com.jnape.palatable.traitor.traits.Trait;
 import okhttp3.Cookie;
+import okhttp3.HttpUrl;
 import okhttp3.Response;
 import org.junit.Assert;
 
 import static com.jnape.palatable.lambda.adt.Maybe.maybe;
-import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.functions.specialized.SideEffect.sideEffect;
 import static org.junit.Assert.assertEquals;
 
 public class SecureRedirect implements Trait<WebRequester<IO<?>, Cookie>> {
     @Override
     public void test(WebRequester<IO<?>, Cookie> requester) {
         requester.requestHttp()
-                .flatMap(response -> assertSecureRedirect(requester, response))
-                .<IO<Unit>>coerce()
+                .fmap(response -> assertSecureRedirect(requester.getHttpsUrl(), response))
+                .<IO<SideEffect>>coerce()
                 .unsafePerformIO();
     }
 
-    private IO<Unit> assertSecureRedirect(WebRequester<IO<?>, Cookie> webRequester, Response response) {
+    private SideEffect assertSecureRedirect(HttpUrl url, Response response) {
         assertEquals(301, response.code());
-        return maybe(response.headers().get("Location")).match(
-                __ -> io((SideEffect) Assert::fail),
-                location -> io(() -> assertEquals(webRequester.getHttpsUrl().toString(), location))
-        );
+        return maybe(response.headers().get("Location"))
+                .fmap(location -> sideEffect(() -> assertEquals(url.toString(), location)))
+                .orElse(Assert::fail);
     }
 }
