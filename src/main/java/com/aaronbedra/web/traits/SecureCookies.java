b0d0c322134646ea675a4eb63647952c462d44c8
Aaron Bedra <aaron@aaronbedra.com>
2020-09-05 09:22:23 -0500

    Refactor

--- src/main/java/com/aaronbedra/web/traits/SecureCookies.java
+++ src/main/java/com/aaronbedra/web/traits/SecureCookies.java
@@ -1,27 +1,27 @@
 package com.aaronbedra.web.traits;
 
 import com.aaronbedra.web.WebRequester;
-import com.jnape.palatable.lambda.adt.Unit;
+import com.jnape.palatable.lambda.functions.specialized.SideEffect;
 import com.jnape.palatable.lambda.io.IO;
 import com.jnape.palatable.traitor.traits.Trait;
 import okhttp3.Cookie;
 
 import java.util.List;
 
-import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.functions.specialized.SideEffect.sideEffect;
 import static org.junit.Assert.assertTrue;
 
 public class SecureCookies implements Trait<WebRequester<IO<?>, Cookie>> {
     @Override
     public void test(WebRequester<IO<?>, Cookie> requester) {
         requester.requestHttps()
-                .flatMap(response -> assertSecure(requester.getCookies(response.request().url())))
-                .<IO<Unit>>coerce()
+                .fmap(response -> assertSecure(requester.getCookies(response.request().url())))
+                .<IO<SideEffect>>coerce()
                 .unsafePerformIO();
     }
 
-    private IO<Unit> assertSecure(List<Cookie> cookies) {
-        return io(() -> cookies.forEach(cookie -> {
+    private SideEffect assertSecure(List<Cookie> cookies) {
+        return sideEffect(() -> cookies.forEach(cookie -> {
             assertTrue(cookie.name() + " not marked secure", cookie.secure());
             assertTrue(cookie.name() + " not marked HttpOnly", cookie.httpOnly());
         }));
